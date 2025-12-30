40843788794835d3446e9bae79eba19d87f3b39e
mefaysal111 <mefaysal111@outlook.com>
2025-12-30 19:28:53 +0530

    add gooru options

--- todo.js
+++ todo.js
@@ -1,22 +1,39 @@
-const addList = document.querySelector('#list');
-const myTodo = document.querySelector('.todo');
-const inputTaskEl = document.querySelector('#text');
-// const deleteButton = document.querySelector('.delete-item');
-const resetButton = document.querySelector('#reset');
-let taskNumber = addList.children.length; // every element has its properties so children is the property of ul and it stores
-// result in the form of array so thats'y we use .length property
-let arrTask = ['Learning HTML', 'Learning CSS'];
+const addList = document.querySelector('#list'); // display list
+const myTodo = document.querySelector('.todo'); // add new task
+const inputTaskEl = document.querySelector('#text'); //enter task
+const priorityEl = document.querySelector('#priority'); //priority tasks
+const resetButton = document.querySelector('#reset'); // reset button
+let arrTask = JSON.parse(localStorage.getItem('tasks')) || [];
+function saveTask() {
+  localStorage.setItem('tasks', JSON.stringify(arrTask));
+}
 function deleteFn(index) {
   arrTask.splice(index, 1);
   renderTask(); //to display changes
+  saveTask();
 }
 function renderTask() {
   addList.innerHTML = '';
   arrTask.forEach((task, index) => {
     //index+1 so that numbering starts at 1 and if we delete some list el numbering changes accordingly
-    const newTask = `<li>${
-      index + 1
-    }.${task}<button class="delete-item" onclick="deleteFn(${index})">Delete</button></li>
+    const newTask = `<li class="task-item">
+    <div class="task-left">
+     <input 
+      type="checkbox"
+      onclick="toggleTask(${index})"
+      ${task.completed ? 'checked' : ''}
+    >
+    <span class="${task.completed ? 'completed' : ''}">
+      ${index + 1}. ${task.text}
+    </span>
+    </div>
+    <div class="task-right">
+     <strong class="priority-${task.priority}">
+      (${task.priority})
+    </strong>
+     <button onclick="editTask(${index})">Edit</button>
+   <button class="delete-item" onclick="deleteFn(${index})">Delete</button>
+   </div></li>
     `;
 
     addList.insertAdjacentHTML('beforeend', newTask);
@@ -28,17 +45,36 @@ myTodo.addEventListener('click', () => {
     alert('Please Enter a Task');
     return;
   }
-  taskNumber++;
-  const newTask = ` ${inputText}`; //here, we are getting task but it is not getting displayed so we  use renderTask
-  // to display each list
-  arrTask.push(newTask); //push task into array
+  arrTask.push({
+    text: inputText,
+    completed: false,
+    priority: priorityEl.value,
+  });
+  saveTask();
+
   renderTask();
   inputTaskEl.value = ''; //make value zero after we add a task
   inputTaskEl.focus(); //focus mouse to input again
 });
-renderTask();
+function toggleTask(index) {
+  arrTask[index].completed = !arrTask[index].completed;
+  saveTask();
+  renderTask();
+}
+function editTask(index) {
+  const newTask = prompt('Edit Task', arrTask[index].text);
+  if (newTask === ' ') return;
+  const newPriority = prompt('Edit priority', arrTask[index].priority);
+  arrTask[index].text = newTask;
+  if (newPriority) {
+    arrTask[index].priority = newPriority;
+  }
+  saveTask();
+  renderTask();
+}
+renderTask(); //initial render
 resetButton.addEventListener('click', () => {
-  taskNumber = 0;
   arrTask = [];
-  addList.innerHTML = '';
+  saveTask();
+  renderTask();
 });
